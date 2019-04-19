a7fda789faad385a2c49387bf1ebefa5f2d5e19d
Rune Holmgren <Raane.Holm@gmail.com>
2019-04-19 17:22:54 +0200

    Adding a test scene to which translates from shadertoy code.

---
+++ src/shaders/balls.glsl
@@ -0,0 +1,49 @@
+uniform float frame;
+uniform vec2 resolution;
+
+void main()
+{
+  vec2 position = gl_FragCoord.xy / resolution;
+
+  gl_FragColor = mainImage(gl_FragCoord.xy, resolution);
+}
+
+float dist(vec2 uv) {
+    return pow(pow(uv.x, 2.) + pow(uv.y, 2.),0.5);
+}
+
+vec4 mainImage(vec2 fragCoord, vec2 iResolution)
+{
+        // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = (fragCoord/iResolution.xy) / vec2(1., 16./9.);
+    uv -= vec2(.5, 9./16./2.);
+    float iTime = frame / 60.;
+    
+
+    // Time varying pixel color
+    float col = 0.;
+    
+    
+    for (float i = 0.; i < 16.; i++) {
+        for (float j = 0.; j < 16.; j++) {
+            
+
+            vec2 gv = vec2(i + mod(j, 2.) / 2. - 8., (j - 8.) * 0.707) / 16.;
+            
+            float size = sin(pow(i-8.,2.)+pow(j-8.,2.)+iTime+25.*dist(uv-gv)) * 0.05;
+            if (dist(uv - gv) < size) {
+                col += 1.;
+            } else {
+                col += 0.;
+            }
+        }
+    }
+    
+    col -= 0.1;
+    col = mod(col, 2.) - .95;
+    
+    // Output to screen
+    fragColor = vec4(vec3(col) ,1.0);
+
+    return fragColor;
+}
